9a996a5d3fbac6703dd880bd4adf1346a9cd6808
Lidor <lidororanim@gmail.com>
2023-03-01 10:30:59 +0200

    fixed README.md and fixed typo in json writing

---
+++ README.md
@@ -0,0 +1,19 @@
+# Data parser from: https://en.wikipedia.org/wiki/List_of_animal_names
+
+This project sends request to the site and getting the data from the last table and represent it in readable way
+in addition it saves the data in txt file and json file
+## installation
+First we need to open the terminal in the project directory\
+then we than we insert the following commands
+
+we create the virtual environment
+
+```python3 -m venv env ```
+
+then we need to activate the virtual environment
+
+```source env/bin/activate  ```
+
+then we need to install the requirements
+
+```pip install -r requirements.txt  ```
--- main.py
+++ main.py
@@ -9,22 +9,6 @@ try:
 
     table_to_filter = parsed_website.findAll('table')[-1]
 
-    page_data = utils.get_data_json_format(table_to_filter).items()
-
-    # because we save the data in the txt file in append mode we need to make sure that the file is empty so the data
-    # won't be duplicated
-    utils.remove_file_if_exists('animals.txt')
-
-    # we loop throw the dictionary and print the animal name and the collateral adjective in prettier way
-    for animal, collateral_adjective in page_data:
-        collateral_adjective = ' '.join(collateral_adjective)
-        print(f'animal: {animal} , collateral adjective: {collateral_adjective}')
-
-        # in this way we create txt file and write the data in it
-        with open('animals.txt', 'a') as file:
-            file.write(f'animal: {animal} , collateral adjective: {collateral_adjective}\n')
-
-    utils.save_data_to_json_file(page_data, 'animals.json')
 
 # this will catch exception if the url is not valid
 except requests.exceptions.MissingSchema as url_error:
@@ -33,6 +17,23 @@ except requests.exceptions.MissingSchema as url_error:
 except IndexError as index_error:
     print('sorry we could not find the table you are looking for')
 # this will catch all teh rest exceptions we put it in order to prevent unhanded exceptions in our program that could
-# make our program crash
+# make our progream crash
 except Exception as error:
     print('some problem occurred please try again later')
+
+page_data = utils.get_data_json_format(table_to_filter).items()
+
+# because we save the data in the txt file in append mode we need to make sure that the file is empty so the data
+# won't be duplicated
+utils.remove_file_if_exists('animals.txt')
+
+# we loop throw the dictionary and print the animal name and the collateral adjective in prettier way
+for animal, collateral_adjective in page_data:
+    collateral_adjective = ' '.join(collateral_adjective)
+    print(f'animal: {animal} , collateral adjective: {collateral_adjective}')
+
+    # in this way we create txt file and write the data in it
+    with open('animals.txt', 'a') as file:
+        file.write(f'animal: {animal} , collateral adjective: {collateral_adjective}\n')
+
+utils.json_file_writer(page_data, 'animals.json')
